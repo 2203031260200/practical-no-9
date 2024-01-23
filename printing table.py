0971bab33bd502d775ea44a4576e71984e1cac61
2203031260200 <157494138+2203031260200@users.noreply.github.com>
2024-01-23 23:53:30 +0530

    Update printing table.py

--- printing table.py
+++ printing table.py
@@ -1,4 +1,9 @@
-a=int(input("enter a:"))
+n=int(input('enter the number:'))
+fh=open('output8.txt','w')
 for i in range(1,11):
-    print(a,'X',i,'=',a*i)
-    i=i+1
+    print(n,'*',i,'=',n*i)
+    t=n*i
+    fh.write(f'{str(n)} * {str(i)} = {str(t)}\n')
+
+fh.close()
+   
